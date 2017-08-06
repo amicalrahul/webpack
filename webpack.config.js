fa360376141f3a415c7bd6c20677bebd612a11ec
Rahul Sabharwal <amicalrahul@gmail.com>
2017-08-06 15:38:01 +1000

    Added a path module
    path module will resolve the paths to bundle js files.
    Moved the html and js files to different folders.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
+var path = require('path');
+
 module.exports = {
+	context: path.resolve('js'),
     entry: ["./app.js","./util.js"],
     output: {
+        path: path.resolve('build/js/'),
+		publicPath: '/public/assets/js/',
         filename: "bundle.js"
     },
+
+	devServer: {
+		contentBase: 'public'
+	},
     watch: true,
     
 	module: {
